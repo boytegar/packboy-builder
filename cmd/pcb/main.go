011599da4495e196f7b539122cc41b3b640a6759
boytegar <works@boytegar.xyz>
2026-09-05 01:21:43 +0700

    chore: bump version to 1.23.8

--- cmd/pcb/main.go
+++ cmd/pcb/main.go
@@ -33,7 +33,7 @@ import (
 	_ "github.com/boytegar/packboy-builder/internal/llm/volcengine"
 )
 
-var version = "1.23.7"
+var version = "1.23.8"
 
 // buildTime and commit are set at build time via -X ldflags.
 // When built directly with go build (without ldflags), they remain empty.
